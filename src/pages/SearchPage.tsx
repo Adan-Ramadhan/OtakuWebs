06801d3a92f460eb7b5bc5771134da66daa7d083
Adan <adanramadhan2001@gmail.com>
2025-10-21 13:08:47 +0700

    update

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -5,7 +5,8 @@ import api from "../service/api"
 const SearchPage = () => {
 
 
-    const [isSearch, setIsSearch] = useState<any>({
+    const [isSearch, setIsSearch] = useState<any>()
+    const [formData, SetFormData] = useState<any>({
         search: ""
     })
     const [isLoading, setIsLoading] = useState<boolean>(false)
@@ -16,24 +17,26 @@ const SearchPage = () => {
     function handleChange(e: any) {
         const { name, value } = e.target
 
-        setIsSearch({
-            ...isSearch,
+        SetFormData({
+            ...formData,
             [name]: value
         })
     }
 
-    function handleSubmit(e) {
+    async function handleSubmit(e) {
         e.preventDefault()
+        console.log("masuk sini")
+        const data = await api.search(formData)
 
-        api.search(isSearch)
-
+        setIsSearch(data)
     }
+    console.log("hasil submit:", isSearch)
 
-    console.log("ini hasil search", isSearch)
+    console.log("ini hasil search", formData)
 
     return (
         <div className="w-full">
-            <div className="w-full p-3">
+            <div className="w-full md:w-3/4 md:mx-auto lg:w-1/2 p-3">
 
                 <h3>Search your fav anime here.</h3>
                 <form onSubmit={handleSubmit} className="border rounded-lg border-slate-500 flex items-center gap-x-2 py-1 px-2">
@@ -41,7 +44,7 @@ const SearchPage = () => {
                     <input
                         type="search"
                         name="search"
-                        value={isSearch.search}
+                        value={formData.search}
                         onChange={handleChange}
                         placeholder="search by title..."
                         className=" focus:outline-none focus:ring-0"
