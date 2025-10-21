86cd02cfb7e9229f088032998e165144637957c3
Adan <adanramadhan2001@gmail.com>
2025-10-21 16:54:55 +0700

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { Link } from "react-router-dom";
 
 type menusType = {
   title: string;
@@ -56,18 +57,21 @@ const Header = () => {
           className={`${isOpen ? " left-0" : "-left-64"
             } fixed top-0 w-64 bg-white shadow-lg transition-all z-10 ease-in-out duration-500 h-full p-3`}
         >
-          <ul className="flex justify-start flex-col gap-y-3 ">
+          <ul className="w-full ">
             <li className="mb-10 overflow-hidden w-20 ">
-                <img src={"./asset/otakuwebs.png"} className="w-full object-center" />
+              <img src={"./asset/otakuwebs.png"} className="w-full object-center" />
             </li>
             {menus.map((menu, i) => (
               <li
                 key={i}
-                className="text-lg font-semibold text-slate-700 hover:text-orange-500 active:text-orange-700"
+                className="text-lg mb-3 font-semibold text-slate-700 hover:text-orange-500 active:text-orange-700"
               >
                 <a href={menu.url}>{menu.title}</a>
               </li>
             ))}
+            <li className="w-full">
+              <a href={"/search"} className="w-full border-2 hover:text-white transition-all ease-in-out duration-300 text-sm md:text-lg border-orange-500 font-bold hover:bg-orange-500 bg-white rounded-full px-4 py-1">Explore</a>
+            </li>
           </ul>
         </nav>
       </div>
